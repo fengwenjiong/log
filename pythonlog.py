db308c591aaadebc10eb2d44cda6f30161378cec
feng wenjiong <956920525@qq.com>
2017-11-20 22:48:45 +0800

    未完

--- pythonlog.py
+++ pythonlog.py
@@ -10,9 +10,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 current_pwd=os.getcwd()
 url = 'https://apis.meican.com/v2.1/closet/downloadRawLog?client_id=TMorVol3uXnalyM7J9s5MMHZdn8HgoM&client_secret=hQaauYWVcZsJR4zEXMdFY4ogo7lsQOT&pathKey=rawlog/closet-383/version-1100473/2017-11-17_20-22/log2017_11_17_20_22.zip'
 print( "downloading...........")
-urllib.request.urlretrieve(url, "/Users/fengwenjiong/Desktop/log_python/log.txt")
-f = open("/Users/fengwenjiong/Desktop/log_python/log.txt")
+urllib.request.urlretrieve(url, os.path.join(os.getcwd(),"logPath.txt"))
+f = open(os.path.join(os.getcwd(),"logPath.txt"))
 line=f.readline()
+os.remove(os.path.join(os.getcwd(),"logPath.txt"))
 print(line)
 data=json.loads(line)
 print(data['data']['url'])
@@ -22,12 +23,13 @@ r = requests.get(parse_url)
 with open("logDownLoad.zip", "wb") as code:
      code.write(r.content)
 
-zipFile = zipfile.ZipFile('/Users/fengwenjiong/Desktop/log_python/logDownLoad.zip')
+zipFile = zipfile.ZipFile(os.path.join(os.getcwd(),"logDownLoad.zip"))
 
 for file in zipFile.namelist():
-  zipFile.extract(file, r'/Users/fengwenjiong/Desktop/log_python')
+  zipFile.extract(file, os.getcwd())
   zipFile.close()
 
-  os.remove('/Users/fengwenjiong/Desktop/log_python/logDownLoad.zip')
+  os.remove(os.path.join(os.getcwd(),"logDownLoad.zip"))
 
+os.system('/Applications/SublimeText.app')
 
