df4a1b7965ee8a01ead2795b0301e794f46d05e4
jiong <fengwenjiong@meican.com>
2017-11-22 16:13:24 +0800

    基本可用(可再优化)

--- pythonlog.py
+++ pythonlog.py
@@ -14,56 +14,47 @@ def mkdir(filename):
         os.mkdir(os.path.join(os.getcwd(), filename))
     #os.chdir(os.path.join(os.getcwd(), filename))
 
-
-def getfilelist(filename1,filename2):
-    final_dir = os.path.join(os.getcwd(), filename1)
+def parse_url(filename,generate_url):
+    final_dir = os.path.join(os.getcwd(), filename)
     url = 'https://apis.meican.com/v2.1/closet/downloadRawLog?client_id=TMorVol3uXnalyM7J9s5MMHZdn8HgoM&client_secret=hQaauYWVcZsJR4zEXMdFY4ogo7lsQOT&pathKey='
-    filelist=sys.argv[2]
-    filelistlink= url + filelist
- #   print(filelistlink)
-    urllib.request.urlretrieve(filelistlink, os.path.join(final_dir, "logList.txt"))
-    list = open(os.path.join(final_dir, "logList.txt"))
-    line=list.readline()
-    os.remove(os.path.join(final_dir, "logList.txt"))
-    print(line)
-    data = json.loads(line)
-    print(data['data']['url'])
-    parse_url = data['data']['url']
-    r = requests.get(parse_url)
-    with open("logDownLoad.zip", "wb") as code:
-        code.write(r.content)
-    zipFile = zipfile.ZipFile(os.path.join(os.getcwd(), "logDownLoad.zip"))
-    for file in zipFile.namelist():
-        zipFile.extract(file, os.getcwd())
-        zipFile.close()
-        os.remove(os.path.join(os.getcwd(), "logDownLoad.zip"))
+    key_url= url + generate_url
+    return key_url
 
+def getfile(filename,mode,generate_url):
+    final_dir = os.path.join(os.getcwd(), filename)
+    key_url=parse_url(filename, generate_url)
+    urllib.request.urlretrieve(key_url, os.path.join(final_dir, "logPath.txt"))
+    list = open(os.path.join(final_dir, "logPath.txt"))
+    line=list.readline()
+    list.close()
+    os.remove(os.path.join(final_dir, "logPath.txt"))
+    #print(line)
+    data = json.loads(line)
+    #print(data['data']['url'])
+    final_url = data['data']['url']
+    r = requests.get(final_url)
+
+    if(mode == "a"):
+        with open(os.path.join(final_dir,"fileList"), "wb") as code:
+            code.write(r.content)
+        loglist = open(os.path.join(final_dir, "fileList"))
+        line= loglist.readlines()
+        for item in line:
+            print(line)
+    else:
+        with open(os.path.join(final_dir, "logDownLoad.zip"), "wb") as code:
+            code.write(r.content)
+        zipFile = zipfile.ZipFile(os.path.join(final_dir, "logDownLoad.zip"))
+
+        for file in zipFile.namelist():
+            zipFile.extract(file, final_dir)
+            zipFile.close()
+
+    os.remove(os.path.join(final_dir,"logDownLoad.zip"))
+    #os.startfile(r'D:\Program Files\IDM Computer Solutions\UltraEdit\uedit64.exe')
+    os.startfile(os.path.join(final_dir, file))
 
 mkdir(sys.argv[1])
-getfilelist(sys.argv[1],sys.argv[2])
+getfile(sys.argv[1],sys.argv[2],sys.argv[3])
 
-url = 'https://apis.meican.com/v2.1/closet/downloadRawLog?client_id=TMorVol3uXnalyM7J9s5MMHZdn8HgoM&client_secret=hQaauYWVcZsJR4zEXMdFY4ogo7lsQOT&pathKey=rawlog/closet-383/version-1100473/2017-11-17_20-22/log2017_11_17_20_22.zip'
-print( "downloading...........")
-urllib.request.urlretrieve(url, os.path.join(os.getcwd(),"logPath.txt"))
-f = open(os.path.join(os.getcwd(),"logPath.txt"))
-line=f.readline()
-os.remove(os.path.join(os.getcwd(),"logPath.txt"))
-print(line)
-data=json.loads(line)
-print(data['data']['url'])
-parse_url=data['data']['url']
-
-r = requests.get(parse_url)
-with open("logDownLoad.zip", "wb") as code:
-     code.write(r.content)
-
-zipFile = zipfile.ZipFile(os.path.join(os.getcwd(),"logDownLoad.zip"))
-
-for file in zipFile.namelist():
-  zipFile.extract(file, os.getcwd())
-  zipFile.close()
-
-  os.remove(os.path.join(os.getcwd(),"logDownLoad.zip"))
-
-#os.system('/Applications/SublimeText.app')
 
